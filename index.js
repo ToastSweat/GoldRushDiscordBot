3804c33b97a4895ab6670146d55581974727a5af
ToastSweat <pada1bobo@gmail.com>
2022-03-20 03:00:00 -0500

    added commands
    added deploy-commands.js

--- index.js
+++ index.js
@@ -16,6 +16,23 @@ client.once('ready', () => {
     sendLogMessage('GOLD RUSH DISCORD BOT STARTED');
 });
 
+
+/** EVENT: CLIENT : INTERACTIONCREATE */
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isCommand()) return;
+
+  const { commandName } = interaction;
+
+  if (commandName === 'ping') {
+    await interaction.reply('Pong!');
+  } else if (commandName === 'server') {
+    await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+  } else if (commandName === 'user') {
+    await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
+  }
+});
+
+
 /** BOT TOKEN */
 client.login(token);
 
