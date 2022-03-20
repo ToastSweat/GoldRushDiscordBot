cceb124e5ef7b089611dade7ed8fe275d948155d
ToastSweat <pada1bobo@gmail.com>
2022-03-19 23:49:38 -0500

    index logging
    added time logging to startup bat, started on a function that will truncate date and time or fill empty space, because why not
    also updated indent in style guide

--- index.js
+++ index.js
@@ -1,18 +1,39 @@
-// Require the necessary discord.js classes
+/** REQUIRE DISCORD.JS CLASSES */
 const { Client, Intents } = require('discord.js');
 const { token } = require('./config.json');
 
-// Create variables
-const Date = new Date();
-
-// Create a new client instance
+/** CREATE CLIENT INSTANCE */
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+/** GLOBAL VARIABLES */
+const LOG_MESSAGE_LENGTH = 13;
+const LOG_MESSAGE_LENGTH_MAX = LOG_MESSAGE_LENGTH - 4;
 
 /** EVENT: CLIENT : READY */
 client.once('ready', () => {
-    console.log('**********************************************************');
-    console.log('* GOLD RUSH DISCORD BOT READY | ' + Date.getDate() + '/' + Date.getMonth() + '/' + Date.getFullYear() + ' *');
-    console.log('**********************************************************');
+    //is this wrong?
+    const DATE = new Date();
+
+    let DATE_STRING = DATE.getMonth() + '/' + DATE.getDate() + '/'  + DATE.getFullYear();
+    let TIME_STRING = DATE.getHours() + ':' + DATE.getMinutes() + ':'  + DATE.getSeconds() + ':'  + DATE.getMilliseconds();
+
+    if (DATE_STRING.length > LOG_MESSAGE_LENGTH_MAX) {
+        DATE_STRING = DATE_STRING.substring(0,LOG_MESSAGE_LENGTH_MAX);
+    }else{
+
+    }
+
+    if (TIME_STRING.length > LOG_MESSAGE_LENGTH_MAX) {
+        TIME_STRING = TIME_STRING.substring(0,LOG_MESSAGE_LENGTH_MAX);
+    }else{
+
+    }
+
+    console.log('*********************************');
+    console.log('* GOLD RUSH DISCORD BOT STARTED *');
+    console.log('* ' + DATE_STRING + ' *');
+    console.log('* ' + TIME_STRING + ' *');
+    console.log('*********************************');
 });
 
 /** BOT TOKEN */
