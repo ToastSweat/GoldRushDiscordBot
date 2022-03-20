53193ee9325204c3edc055bcb8c55c0def4d63f9
ToastSweat <pada1bobo@gmail.com>
2022-03-20 18:34:46 -0500

    commands/events
    added some commands and events handling for texting based off the discord.js tutorial

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 /** REQUIRE DISCORD.JS CLASSES */
-const { Client, Intents } = require('discord.js');
+const fs = require('node:fs');
+const { Client, Collection, Intents } = require('discord.js');
 const { token } = require('./config.json');
+
 
 /** CREATE CLIENT INSTANCE */
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.data.name, command);
+}
+
+
 
 /** GLOBAL VARIABLES */
 const LOG_MESSAGE_LENGTH = 80;
@@ -12,23 +24,27 @@ const LOG_MESSAGE_LENGTH_MAX = LOG_MESSAGE_LENGTH - 4;
 
 
 /** EVENT: CLIENT : READY */
-client.once('ready', () => {
-    sendLogMessage('GOLD RUSH DISCORD BOT STARTED');
+client.once('ready', (c) => {
+  sendLogMessage('LOGGED IN AS : ' + c.user.tag);
+  sendLogMessage('GOLD RUSH DISCORD BOT STARTED');
 });
 
 
 /** EVENT: CLIENT : INTERACTIONCREATE */
 client.on('interactionCreate', async interaction => {
+  console.log(`${interaction.user.tag} in #${interaction.channel.name} triggered an interaction.`);
+
   if (!interaction.isCommand()) return;
 
-  const { commandName } = interaction;
+  const command = client.commands.get(interaction.commandName);
 
-  if (commandName === 'ping') {
-    await interaction.reply('Pong!');
-  } else if (commandName === 'server') {
-    await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
-  } else if (commandName === 'user') {
-    await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
+  if (!command) return;
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
   }
 });
 
@@ -40,8 +56,7 @@ function sendLogMessage(message) {
     //is this wrong?
     const DATE = new Date();
 
-    let DATE_TIME_STRING = DATE.getMonth() + '/' + DATE.getDate() + '/'  + DATE.getFullYear() + ' | ' +
-      DATE.getHours() + ':' + DATE.getMinutes() + ':'  + DATE.getSeconds() + ':'  + DATE.getMilliseconds();
+    let DATE_TIME_STRING = DATE.getHours() + ':' + DATE.getMinutes() + ':'  + DATE.getSeconds() + ':'  + DATE.getMilliseconds() + ' | ' + DATE.getMonth() + '/' + DATE.getDate() + '/'  + DATE.getFullYear();
 
     function truncateLogString(log) {
       if (log.length > LOG_MESSAGE_LENGTH_MAX) {
